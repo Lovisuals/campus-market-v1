bbb8aa36d366dea99e2765ac67e4377bf0bf9b24
Lovisuals <130866714+Lovisuals@users.noreply.github.com>
2025-12-26 13:52:41 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Campus Market P2P",
+  description: "The fastest way to  sell as a student.NO SIGNUP,JUST BUY/SELL",
 };
 
 const geistSans = Geist({
